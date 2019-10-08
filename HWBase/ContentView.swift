ced55f3986eddd18343db8f2f7ae41753aebc556
陈志文 <434790913@qq.com>
2019-10-08 17:30:13 +0800

    Initial Commit

---
+++ HWBase/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HWBase
+//
+//  Created by Harwin on 2019/10/8.
+//  Copyright © 2019 MyCompany. All rights reserved.
+//
+
+import SwiftUI
+
+/*
+ struct ContentView: View {
+ var body: some View {
+     Text("Hello World")
+ }
+ }
+
+ struct ContentView_Previews: PreviewProvider {
+ static var previews: some View {
+     ContentView()
+ }
+ }
+ */
---
+++ HWBase/HWMeidiator.swift
@@ -0,0 +1,19 @@
+//
+//  HWMeidiator.swift
+//  HWBase
+//
+//  Created by Harwin on 2019/10/8.
+//  Copyright © 2019 MyCompany. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HWMeidiator: NSObject {
+    func perform(target:String,action:String,parameters:Dictionary<String, Any>) -> Any {
+//        let targetClassString =
+        let targetClass = NSClassFromString(target) as? UIViewController
+        let tar = targetClass.alloc.ini
+        return tar;
+    }
+}
