29d96e6954273e95cd2c89bd12123c7448f0fa69
陈志文 <434790913@qq.com>
2019-10-09 09:37:10 +0800

    创建

--- HWBase/HWMeidiator.swift
+++ HWBase/HWMeidiator.swift
@@ -12,8 +12,8 @@ import UIKit
 class HWMeidiator: NSObject {
     func perform(target:String,action:String,parameters:Dictionary<String, Any>) -> Any {
 //        let targetClassString =
-        let targetClass = NSClassFromString(target) as? UIViewController
-        let tar = targetClass.alloc.ini
-        return tar;
+//        let targetClass = NSClassFromString(target) as? UIViewController
+//        let tar = targetClass.alloc.ini
+        return Any;
     }
 }
